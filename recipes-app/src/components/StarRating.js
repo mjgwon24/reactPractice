7f2b6ea0a0f3bc085fb784849fd64d8b43635def
minji <alswlchlrh8@naver.com>
2024-08-23 06:59:44 +0900

    commit

--- recipes-app/src/components/StarRating.js
+++ recipes-app/src/components/StarRating.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaStar} from "react-icons/fa";
 
-export default function StarRating() {
+const Star = ({ selected = false, onSelect = f => console.log("click") }) => (
+    <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+// length 길이의 빈 배열 생성
+const createArray = length => [...Array(length)];
+
+export default function StarRating({style= {}, totalStars = 5, ...props}) {
+    // useState: React의 상태 관리 훅, 컴포넌트 내에서 상태 변수를 선언하고 그 값을 업데이트할 수 있게 해준다.
+    // 반환 배열 2개: 현재 상태 값, 상태 업데이트 함수
+    // useState 훅 데이터 변경 -> 훅이 걸린 컴포넌트에 새 값 전달 -> 컴포넌트 재렌더링
+    const [selectedStars, setSelectedStars] = useState(0);
+
     // return []: 여러개의 JSX 요소를 배열로 반환, 주로 동적으로 여러 요소를 생성할 때 사용
-    return [
-      <FaStar color="red" />,
-      <FaStar color="red" />,
-      <FaStar color="red" />,
-      <FaStar color="red" />,
-      <FaStar color="red" />
-    ];
+    // return (): JSX 요소 반환, 하나의 루트 요소 포함, 일반적인 컴포넌트 구조에서 사용
+    return(
+      <div style={{padding: "5px", ...style}} {...props}>
+          {createArray(totalStars).map((_, i) => (
+              <Star
+                  key={i}
+                  selected={selectedStars > i}
+                  onSelect={() => setSelectedStars(i + 1)}
+              />
+          ))}
+          <p>{selectedStars} / {totalStars}</p>
+      </div>
+    );
 }
--- recipes-app/src/index.js
+++ recipes-app/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import {render} from "react-dom";
 import Menu from "./components/Menu";
 import data from "./data/recipes.json";
+import StarRating from "./components/StarRating";
 
 render(
-    <Menu recipes={data}/>,
+    <div>
+        <StarRating
+            totalStars={7}
+            style={{backgroundColor: "yellow"}}
+            onDoubleClick={e => alert("double click")}
+        />
+        <Menu recipes={data}/>
+    </div>,
     document.getElementById("root")
 );
