c6da62b4ec9158c48fcb2d114d1a7aa23908269a
minji <alswlchlrh8@naver.com>
2024-08-28 11:40:35 +0900

    상호작용을 component트리 위쪽으로 전달 - 색상 제거 클릭이벤트 추가

--- recipes-app/src/components/ColorList.js
+++ recipes-app/src/components/ColorList.js
@@ -1,26 +1,30 @@
 import React from "react";
 import StarRating from "./StarRating";
+import {FaTrash} from "react-icons/fa";
 
-export function Color({title, color, rating}) {
+export function Color({id, title, color, rating, onRemove = f => f}) {
     return (
         <section>
             <h1>{title}</h1>
+            <button onClick={() => onRemove(id)}>
+                <FaTrash />
+            </button>
             <div style={{height: 50, backgroundColor: color}} />
             <StarRating selectedStars={rating}/>
         </section>
     );
 };
 
-export default function ColorList({colors = []}) {
+export default function ColorList({colors = [], onRemoveColor = f => f}) {
     if (!colors.length) {
         return <div>표시할 색이 없습니다.</div>;
     }
 
     return (
         <div>
-            {
-                colors.map(color => <Color key={color.id} {...color} />)
-            }
+            { colors.map(color => (
+                <Color key={color.id} {...color} onRemove={onRemoveColor}/>
+            ))}
         </div>
     )
 };
