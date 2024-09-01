4d14ca0048c22d51832b9b54f2f0e488dc7efbcc
minji <alswlchlrh8@naver.com>
2024-09-01 13:49:44 +0900

    색상 추가 컴포넌트 App.js return에 추가

--- recipes-app/src/components/App.js
+++ recipes-app/src/components/App.js
@@ -1,28 +1,44 @@
 import React, {useState} from "react";
 import colorData from "../data/color-data.json";
 import ColorList from "./ColorList";
+import AddColorForm from "./AddColorForm";
 
 export default function App() {
-    // useState: React의 상태 관리 훅, 컴포넌트 내에서 상태 변수를 선언하고 그 값을 업데이트할 수 있게 해준다.
-    // 반환 배열 2개: 현재 상태 값, 상태 업데이트 함수
-    // useState 훅 데이터 변경 -> 훅이 걸린 컴포넌트에 새 값 전달 -> 컴포넌트 재렌더링
     const [colors, setColors] = useState(colorData.colors);
 
-    // return []: 여러개의 JSX 요소를 배열로 반환, 주로 동적으로 여러 요소를 생성할 때 사용
-    // return (): JSX 요소 반환, 하나의 루트 요소 포함, 일반적인 컴포넌트 구조에서 사용
     return (
-        <ColorList
-            colors = {colors}
-            onRemoveColor={id => {
-                const newColors = colors.filter(color => color.id !== id);
-                setColors(newColors);
-            }}
-            onRateColor={(id, rating) => {
-                const newColors = colors.map(color =>
-                    color.id === id ? {...color, rating} : color
-                );
-                setColors(newColors);
-            }}
-        />
+        <>
+            <AddColorForm
+                onNewColor={(title, color) => {
+                    // colorData.colors 배열에서 마지막 요소의 id
+                    const lastColorId = colors[colors.length - 1].id;
+                    const newId = String(parseInt(lastColorId) + 1);
+
+                    const newColors = [
+                        ...colors,
+                        {
+                            "id": newId,
+                            "title": title,
+                            "color": color,
+                            "rating": 0
+                        }
+                    ];
+                    setColors(newColors);
+                }}
+            />
+            <ColorList
+                colors = {colors}
+                onRemoveColor={id => {
+                    const newColors = colors.filter(color => color.id !== id);
+                    setColors(newColors);
+                }}
+                onRateColor={(id, rating) => {
+                    const newColors = colors.map(color =>
+                        color.id === id ? {...color, rating} : color
+                    );
+                    setColors(newColors);
+                }}
+            />
+        </>
     )
 }
--- recipes-app/src/index.js
+++ recipes-app/src/index.js
@@ -4,15 +4,9 @@ import Menu from "./components/Menu";
 import data from "./data/recipes.json";
 import StarRating from "./components/StarRating";
 import App from "./components/App";
-import AddColorForm from "./components/AddColorForm";
 
 render(
     <div>
-        <AddColorForm
-            onNewColor={(title, color) => {
-                console.log(`ADD버튼 클릭!!! title: ${title}, color: ${color}`);
-            }}
-        />
         <App />
         <StarRating
             totalStars={7}
